712146d1c34489bef5d0df84c0b644fcfefa93a5
ernesto <evazoliver@gmail.com>
2026-04-21 22:27:32 -0300

    feat: create boilerplate

---
+++ app/Main.hs
@@ -0,0 +1,35 @@
+module Main where
+
+import Options.Applicative
+
+data Options = Options
+  { verbose :: Bool
+  , input   :: String
+  }
+
+parser :: Parser Options
+parser = Options
+  <$> switch
+      ( long "verbose"
+     <> short 'v'
+     <> help "Enable verbose mode" )
+  <*> strOption
+      ( long "input"
+     <> short 'i'
+     <> metavar "TEXT"
+     <> help "Input text" )
+
+main :: IO ()
+main = scream =<< execParser opts
+  where
+    opts = info (parser <**> helper)
+      ( fullDesc
+     <> progDesc "Scream the input text"
+     <> header "scream - a simple command-line tool" )
+
+scream :: Options -> IO ()
+scream opts = do
+  let message = if verbose opts
+                then "Screaming: " ++ input opts ++ "!!!"
+                else input opts ++ "!!!"
+  putStrLn message
