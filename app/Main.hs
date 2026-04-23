17cb86b0b4f89dbd1a91886c8107fb8deefed5ae
ernesto <evazoliver@gmail.com>
2026-04-23 03:49:55 -0300

    feat: use explicit get method

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,8 @@
 module Main (main) where
 
-import Network.HTTP.Client
-import Network.HTTP.Client.TLS
+import Network.HTTP.Client(newManager, parseRequest, httpLbs, responseStatus, responseBody, method)
+import Network.HTTP.Client.TLS(tlsManagerSettings)
+import qualified Data.ByteString.Char8 as C8
 
 import qualified CommandlineParser
 
@@ -13,7 +14,8 @@ main = do
 app :: CommandlineParser.Arguments -> IO ()
 app args = do
   manager <- newManager tlsManagerSettings
-  request <- parseRequest $ CommandlineParser.url args
+  baseRequest <- parseRequest $ CommandlineParser.url args
+  let request = baseRequest { method = C8.pack "GET" }
   response <- httpLbs request manager
 
   putStrLn $ "The status code was: " ++ show (responseStatus response)
